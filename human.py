ea8a50c50cc438eece2308686addaeff90fb412c
RegiHunter <chrishunter321work@gmail.com>
2021-09-13 14:35:44 -0600

    Testing of renaming files

--- human.py
+++ human.py
@@ -1,6 +1,3 @@
-from player import Player
-
 class Human(Player):
     def __init__(self):
         self.name = input("Name your player!")
-        super(). __init__()   
--- player.py
+++ player.py
@@ -1,16 +1,6 @@
 class Player:
     def __init__(self):
-        self.gestures = ['rock', 'paper', 'scissors', 'lizard', 'spock']
-        self.selected_gesture = ''
-        self.score = 0
+        self.name = ""
 
-    def enter_name(self):
-        self.name = input('Enter player name.')
-
-    def select_gesture(self):
-        selected_gesture = input(f'Select your fighter!: {self.gestures[0]}, {self.gestures[1]}, {self.gestures[2]}, {self.gestures[3]}, {self.gestures[4]}').lower()
-        if selected_gesture == self.gestures:
-            self.selected_gesture = selected_gesture
-            print(f'{self.name} has selected {self.selected_gesture}.')
-        else:
-            self.select_gesture()   
+    def play_hand(self):
+        pass
