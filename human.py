b5cd3fd0fc5357a6fef13a9b25ab67a238706902
ParkerHadley <89861123+ParkerHadley@users.noreply.github.com>
2021-09-13 11:32:46 -0600

    Adding clarifying messages to human.py for use of super()

--- human.py
+++ human.py
@@ -1,5 +1,5 @@
 from Player import player
-
+### calling __init__() of playr class allowing it to be used in the human class without repeating code
 class human(player):
     def __init__(self):
-        super(). __init__()
+        super(). __init__()   
