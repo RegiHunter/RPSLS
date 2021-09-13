920d8fa607b1f2685617dc7dbc877fa5d3c5b95d
ParkerHadley <89861123+ParkerHadley@users.noreply.github.com>
2021-09-13 15:11:05 -0600

    Updates to names of files

--- Human.py
+++ Human.py
@@ -1,3 +1,6 @@
+from player import Player
+
 class Human(Player):
     def __init__(self):
         self.name = input("Name your player!")
+        super(). __init__()  
--- Player.py
+++ Player.py
@@ -4,3 +4,14 @@ class Player:
 
     def play_hand(self):
         pass
+
+    def enter_name(self):
+        self.name = input('Enter player name.')
+
+    def select_gesture(self):
+        selected_gesture = input(f'Select your fighter!: {self.gestures[0]}, {self.gestures[1]}, {self.gestures[2]}, {self.gestures[3]}, {self.gestures[4]}').lower()
+        if selected_gesture == self.gestures:
+            self.selected_gesture = selected_gesture
+            print(f'{self.name} has selected {self.selected_gesture}.')
+        else:
+            self.select_gesture()    
