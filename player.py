372bfe408a7cdaff392cc8153b95cc0fd36e15f5
ParkerHadley <89861123+ParkerHadley@users.noreply.github.com>
2021-09-13 10:54:52 -0600

    Initial defining of parent class: Player

---
+++ player.py
@@ -0,0 +1,8 @@
+class Player:
+    def __init__(self):
+        self.name = ''
+        self.gestures = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+        self.selected_gesture = ''
+        self.score = 0
+
+  
