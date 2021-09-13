e61fb5957c9e00726be28d06e835065bac6c0f5b
ParkerHadley <89861123+ParkerHadley@users.noreply.github.com>
2021-09-13 12:12:36 -0600

    update to proper markings for messages on main.py

--- Main.py
+++ Main.py
@@ -1,8 +1,8 @@
-### Date:       09/13/2021
-### Project:    RPSLS
-### For:        devCodeCamp
-### Developers  Regie Hunter && Parker Hadley
+# Date:       09/13/2021
+# Project:    RPSLS
+# For:        devCodeCamp
+#  Developers  Regie Hunter && Parker Hadley
 
-### Purpose of project: To create a game of rock, paper, scissor, lizard, spock (idea imported from Big Bang Theory). This game will run best out of # and will give you the option of playing single player(against AI) or multiplayer(against another human player).
+""" Purpose of project: To create a game of rock, paper, scissor, lizard, spock (idea imported from Big Bang Theory). This game will run best out of # and will give you the option of playing single player(against AI) or multiplayer(against another human player)."""
 
 
