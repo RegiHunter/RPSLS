8ca09d9d55ce232b377ec69a0d189f524950afef
ParkerHadley <89861123+ParkerHadley@users.noreply.github.com>
2021-09-13 11:11:23 -0600

    Linking gesture selection to prompt for user input

--- player.py
+++ player.py
@@ -1,4 +1,4 @@
-class Player:
+class player:
     def __init__(self):
         self.name = ''
         self.gestures = ['rock', 'paper', 'scissors', 'lizard', 'spock']
@@ -8,4 +8,11 @@ class Player:
     def enter_name(self):
         self.name = input('Enter player name.')
 
+    def select_gesture(self):
+        selected_gesture = input(f'Select your fighter!: {self.gestures[0]}, {self.gestures[1]}, {self.gestures[2]}, {self.gestures[3]}, {self.gestures[4]}').lower()
+        if selected_gesture == self.gestures:
+            self.selected_gesture = selected_gesture
+            print(f'{self.name} has selected {self.selected_gesture}.')
+        else:
+            self.select_gesture()
 
