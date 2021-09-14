a552ad52ba9b94b47a934eb3c707107ad82c54bb
RegiHunter <chrishunter321work@gmail.com>
2021-09-14 09:24:06 -0600

    .

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Date:       09/13/2021
 # Project:    RPSLS
 # For:        devCodeCamp
-#  Developers  Regie Hunter && Parker Hadley
+#  Developers  Regi Hunter && Parker Hadley
 
 """ Purpose of project: To create a game of rock, paper, scissor, lizard, spock (idea imported from Big Bang Theory). This game will run best out of # and will give you the option of playing single player(against AI) or multiplayer(against another human player)."""
 
